3747efd5ab55fe8f4f48ad3c298c9f4c5862fff2
yaerhee <yaerhee@naver.com>
2022-10-17 23:31:44 +0900

    quiz tips

---
+++ study/quiz/day1.js
@@ -0,0 +1,10 @@
+// 1. JS -> Not Type Safe
+// 2. const test = ['hello']+{hi:1}
+// 3. runtime error = 코드 실행 시 발생하는 에러
+// 4. JavaScript -> compiled -> TypeScript
+// 5. const kimchi = {
+//     맛있어: true
+// }
+//
+// const test = kimchi.맛있어
+// 7. const x: string = 'hi'
